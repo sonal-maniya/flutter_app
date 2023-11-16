d5a15b2461a3e5ef1f27d677b4ac99e0e172bf7a
sonal-maniya <sonal.maniya@hubilo.com>
2023-11-16 16:30:01 +0530

    feat: appbar

--- lib/layout.dart
+++ lib/layout.dart
@@ -10,70 +10,76 @@ class BasicWidget extends StatefulWidget {
 class _BasicWidgetState extends State<BasicWidget> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(children: [
-        Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                _buildContainer(Colors.purple),
-                const Spacer(),
-                _buildContainer(Colors.amberAccent),
-                const Spacer(),
-                _buildContainer(Colors.white38),
-                const Spacer(),
-                _buildContainer(Colors.cyanAccent),
-              ],
-            )),
-        const SizedBox(
-          height: 20,
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.amberAccent,
-            child: Wrap(
-              runSpacing: 20,
-              spacing: 10,
-              alignment: WrapAlignment.start,
-              children: [
-                _buildText('Text 1'),
-                _buildText('Text 2'),
-                _buildText('Text 3'),
-                _buildText('Text 4'),
-                _buildText('Text 5'),
-                _buildText('Text 6'),
-                _buildText('Text 7'),
-                _buildText('Text 8'),
-                _buildText('Text 9'),
-                _buildText('Text 10'),
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+        title: const Text("Basic Layouts"),
+      ),
+      body: SafeArea(
+        child: Column(children: [
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  _buildContainer(Colors.purple),
+                  const Spacer(),
+                  _buildContainer(Colors.amberAccent),
+                  const Spacer(),
+                  _buildContainer(Colors.redAccent),
+                  const Spacer(),
+                  _buildContainer(Colors.cyanAccent),
+                ],
+              )),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+              child: Wrap(
+                runSpacing: 20,
+                spacing: 10,
+                alignment: WrapAlignment.start,
+                children: [
+                  _buildText('Text 1'),
+                  _buildText('Text 2'),
+                  _buildText('Text 3'),
+                  _buildText('Text 4'),
+                  _buildText('Text 5'),
+                  _buildText('Text 6'),
+                  _buildText('Text 7'),
+                  _buildText('Text 8'),
+                  _buildText('Text 9'),
+                  _buildText('Text 10'),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Expanded(
-          flex: 1,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              _buildText('Row Text 1'),
-              _buildText('Row Text 2'),
-              _buildText('Row Text 3'),
-            ],
+          const SizedBox(
+            height: 20,
           ),
-        )
-      ]),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                _buildText('Row Text 1'),
+                _buildText('Row Text 2'),
+                _buildText('Row Text 3'),
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 
   Widget _buildText(String text) {
     return Text(
       text,
-      style: TextStyle(color: Colors.white, fontSize: 20),
+      style: const TextStyle(color: Colors.black, fontSize: 20),
     );
   }
 
