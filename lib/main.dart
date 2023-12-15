5e86760f8eeab83eebaa117822ec1564829af606
sonal-maniya <sonal.maniya@hubilo.com>
2023-12-15 13:22:29 +0530

    feat: counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/constants/route_constants.dart';
-import 'package:flutter_app/routes/routing.dart';
-import 'package:flutter_app/stateManagement/provider/count_provider.dart';
-import 'package:flutter_app/stateManagement/provider/example_one_provider.dart';
-import 'package:flutter_app/stateManagement/provider/favourite_provider.dart';
-import 'package:flutter_app/stateManagement/provider/login_provider.dart';
-import 'package:flutter_app/stateManagement/provider/theme_provider.dart';
-import 'package:flutter_app/stateManagement/screen/count_example.dart';
-import 'package:flutter_app/stateManagement/screen/example_one.dart';
-import 'package:flutter_app/stateManagement/screen/favourite/dark_theme.dart';
-import 'package:flutter_app/stateManagement/screen/favourite/favourite_page.dart';
-import 'package:flutter_app/stateManagement/screen/favourite/value_notifier_page.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final counterStateProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
+// River
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => LoginProvider()),
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: Builder(builder: (context) {
-        final themeProvider = Provider.of<ThemeProvider>(context);
-        return MaterialApp(
-          title: 'Flutter Demo',
-          themeMode: themeProvider.themeMode,
-          theme: ThemeData(
-              primaryColor: Colors.white,
-              brightness: Brightness.light,
-              primarySwatch: Colors.blue,
-              appBarTheme: const AppBarTheme(
-                  backgroundColor: Color.fromARGB(255, 247, 246, 246),
-                  foregroundColor: Colors.black),
-              primaryColorDark: Colors.black,
-              primaryColorLight: Colors.white,
-              canvasColor: Colors.white),
-          darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            primaryColor: Colors.purple,
-            primarySwatch: Colors.blue,
-            appBarTheme: const AppBarTheme(
-                backgroundColor: Color.fromARGB(255, 45, 43, 43),
-                foregroundColor: Colors.white),
-            primaryColorDark: const Color.fromARGB(255, 45, 43, 43),
-            primaryColorLight: Colors.white,
-            canvasColor: const Color.fromARGB(255, 45, 43, 43),
+  Widget build(BuildContext context, WidgetRef ref) {
+    var value = ref.watch(counterStateProvider);
+    return MaterialApp(
+      title: "Riverpod demo",
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: Text(
+              "Value ${value.toString()}",
+              style: const TextStyle(fontSize: 30),
+            )),
+          ],
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  ref.read(counterStateProvider.notifier).state++;
+                },
+                child: const Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  ref.read(counterStateProvider.notifier).state--;
+                },
+                child: const Icon(Icons.remove),
+              ),
+            ],
           ),
-          debugShowCheckedModeBanner: false,
-          onGenerateRoute: Routing.onGenerateRoute,
-          initialRoute: RouteConstants.initialPage,
-        );
-      }),
+        ),
+      ),
     );
-  }
-}
-
-class MyProviderApp extends StatelessWidget {
-  const MyProviderApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => CountProvider()),
-          ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
-          ChangeNotifierProvider(create: (_) => FavoriteProvider()),
-          ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ],
-        child: Builder(builder: (BuildContext context) {
-          final themeProvider = Provider.of<ThemeProvider>(context);
-          return MaterialApp(
-            title: 'Flutter Demo',
-            themeMode: themeProvider.themeMode,
-            theme: ThemeData(
-              primaryColor: Colors.white,
-              brightness: Brightness.light,
-              primarySwatch: Colors.amber,
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              primaryColor: Colors.purple,
-              primarySwatch: Colors.blue,
-              appBarTheme: const AppBarTheme(backgroundColor: Colors.teal),
-            ),
-            debugShowCheckedModeBanner: false,
-            home: ValueNotifierWidget(),
-          );
-        }));
   }
 }
---
+++ lib/main_insta.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/constants/route_constants.dart';
+import 'package:flutter_app/routes/routing.dart';
+import 'package:flutter_app/stateManagement/provider/count_provider.dart';
+import 'package:flutter_app/stateManagement/provider/example_one_provider.dart';
+import 'package:flutter_app/stateManagement/provider/favourite_provider.dart';
+import 'package:flutter_app/stateManagement/provider/login_provider.dart';
+import 'package:flutter_app/stateManagement/provider/theme_provider.dart';
+import 'package:flutter_app/stateManagement/screen/count_example.dart';
+import 'package:flutter_app/stateManagement/screen/example_one.dart';
+import 'package:flutter_app/stateManagement/screen/favourite/dark_theme.dart';
+import 'package:flutter_app/stateManagement/screen/favourite/favourite_page.dart';
+import 'package:flutter_app/stateManagement/screen/favourite/value_notifier_page.dart';
+import 'package:provider/provider.dart';
+
+// River
+void main() {
+  runApp(const MyApp());
+}
+
+void main1() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LoginProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: Builder(builder: (context) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return MaterialApp(
+          title: 'Flutter Demo',
+          themeMode: themeProvider.themeMode,
+          theme: ThemeData(
+              primaryColor: Colors.white,
+              brightness: Brightness.light,
+              primarySwatch: Colors.blue,
+              appBarTheme: const AppBarTheme(
+                  backgroundColor: Color.fromARGB(255, 247, 246, 246),
+                  foregroundColor: Colors.black),
+              primaryColorDark: Colors.black,
+              primaryColorLight: Colors.white,
+              canvasColor: Colors.white),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.purple,
+            primarySwatch: Colors.blue,
+            appBarTheme: const AppBarTheme(
+                backgroundColor: Color.fromARGB(255, 45, 43, 43),
+                foregroundColor: Colors.white),
+            primaryColorDark: const Color.fromARGB(255, 45, 43, 43),
+            primaryColorLight: Colors.white,
+            canvasColor: const Color.fromARGB(255, 45, 43, 43),
+          ),
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: Routing.onGenerateRoute,
+          initialRoute: RouteConstants.initialPage,
+        );
+      }),
+    );
+  }
+}
+
+class MyProviderApp extends StatelessWidget {
+  const MyProviderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => CountProvider()),
+          ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
+          ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ],
+        child: Builder(builder: (BuildContext context) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+          return MaterialApp(
+            title: 'Flutter Demo',
+            themeMode: themeProvider.themeMode,
+            theme: ThemeData(
+              primaryColor: Colors.white,
+              brightness: Brightness.light,
+              primarySwatch: Colors.amber,
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primaryColor: Colors.purple,
+              primarySwatch: Colors.blue,
+              appBarTheme: const AppBarTheme(backgroundColor: Colors.teal),
+            ),
+            debugShowCheckedModeBanner: false,
+            home: ValueNotifierWidget(),
+          );
+        }));
+  }
+}
